b59e6f33bb0b80c0a7352319c0a98bb0cb497df7
Priyanshu Rajput <priyanshurajput@itechindia.co>
2023-06-15 15:35:56 +0530

    it is done

---
+++ Annotations/test.py
@@ -0,0 +1,472 @@
+import json
+
+json_data = {
+
+  "images": [
+    {
+      "width": 1100,
+      "height": 850,
+      "id": 0,
+      "file_name": "images\/6aff52b9-Layout_2_ABB_001_page-1.png"
+    },
+    {
+      "width": 1655,
+      "height": 1170,
+      "id": 1,
+      "file_name": "images\/e867baf3-Layout_3_InkedConverteam_002_LI_page-1.png"
+    },
+    {
+      "width": 1655,
+      "height": 1170,
+      "id": 2,
+      "file_name": "images\/06a294e5-Layout_3_InkedConverteam_002_LI_page-11.png"
+    },
+    {
+      "width": 1573,
+      "height": 1125,
+      "id": 3,
+      "file_name": "images\/39d4753a-Layout_3_InkedDrever_001_002_LI_page-1.png"
+    },
+    {
+      "width": 1698,
+      "height": 1099,
+      "id": 4,
+      "file_name": "images\/010e2db1-Layout_3_InkedJNE_Consulting_001_002_LI_page-8.png"
+    },
+    {
+      "width": 1654,
+      "height": 1169,
+      "id": 5,
+      "file_name": "images\/d5272d8e-Layout_3_InkedDemagCranes_LI_page-1.png"
+    },
+    {
+      "width": 1698,
+      "height": 1099,
+      "id": 6,
+      "file_name": "images\/f9cc8903-Layout_3_InkedJNE_Consulting_001_002_LI_page-1.png"
+    },
+    {
+      "width": 3312,
+      "height": 2339,
+      "id": 7,
+      "file_name": "images\/f9c88270-Layout_3_InkedABB_003_LI_page-1.png"
+    },
+    {
+      "width": 4682,
+      "height": 3312,
+      "id": 8,
+      "file_name": "images\/848776b4-Layout_3_InkedConverteam_001_LI_page-1.png"
+    },
+    {
+      "width": 1573,
+      "height": 1125,
+      "id": 9,
+      "file_name": "images\/1cf177db-Layout_3_InkedConverteam_002_002_LI_page-127.png"
+    },
+    {
+      "width": 1170,
+      "height": 827,
+      "id": 10,
+      "file_name": "images\/4137159e-Layout_3_InkedInductotherm_004_002_LI_page-6.png"
+    },
+    {
+      "width": 1700,
+      "height": 1098,
+      "id": 11,
+      "file_name": "images\/c6e8fe71-Layout_3_InkedInkedFOEN_002_002_LI_page-6.png"
+    },
+    {
+      "width": 3312,
+      "height": 2339,
+      "id": 12,
+      "file_name": "images\/2e1130f9-Layout_3_InkedJNE_Consulting_002_LI_page-1.png"
+    },
+    {
+      "width": 3034,
+      "height": 2346,
+      "id": 13,
+      "file_name": "images\/27a198eb-Layout_3_InkedInductotherm_002_LI_page-1.png"
+    },
+    {
+      "width": 1573,
+      "height": 1125,
+      "id": 14,
+      "file_name": "images\/bee84bad-Layout_3_InkedConverteam_004_LI_page-1.png"
+    },
+    {
+      "width": 1573,
+      "height": 1125,
+      "id": 15,
+      "file_name": "images\/ab8b1aba-Layout_3_InkedDuma_001_002_LI_page-26.png"
+    },
+    {
+      "width": 1655,
+      "height": 1170,
+      "id": 16,
+      "file_name": "images\/dd58a449-Layout_3_InkedConverteam_003_002_LI_page-2.png"
+    },
+    {
+      "width": 1573,
+      "height": 1125,
+      "id": 17,
+      "file_name": "images\/9ec6bb55-Layout_3_InkedConverteam_004_LI_page-75.png"
+    },
+    {
+      "width": 1100,
+      "height": 850,
+      "id": 18,
+      "file_name": "images\/d003b61f-Layout_3_InkedFOEN_001_002_LI_page-1.png"
+    },
+    {
+      "width": 2339,
+      "height": 1653,
+      "id": 19,
+      "file_name": "images\/5c2a094d-Layout_3_InkedFAGOR_002_LI_page-1.png"
+    }
+  ],
+  "categories": [
+    {
+      "id": 0,
+      "name": "Table"
+    }
+  ],
+  "annotations": [
+    {
+      "id": 0,
+      "image_id": 0,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        269.3379912143866,
+        312.7554505298676,
+        496.25446061404966,
+        198.06884687540045
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 98292.54877059865
+    },
+    {
+      "id": 1,
+      "image_id": 1,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        133.5483870967742,
+        310.64516129032256,
+        1421.1290322580649,
+        780.9677419354839
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 1109855.9313215404
+    },
+    {
+      "id": 2,
+      "image_id": 2,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        670.6451612903226,
+        854.9999999999999,
+        878.2258064516129,
+        235.16129032258078
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 206524.7138397504
+    },
+    {
+      "id": 3,
+      "image_id": 3,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        774.6588089330024,
+        928.1947890818858,
+        770.4714640198512,
+        185.63895781637726
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 143029.51960790358
+    },
+    {
+      "id": 4,
+      "image_id": 4,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        782.0663900414934,
+        697.5186721991709,
+        655.2448132780087,
+        324.0992713788265
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 212364.3665581578
+    },
+    {
+      "id": 5,
+      "image_id": 5,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        854.2692307692308,
+        762.8957816377172,
+        704.880893300248,
+        321.9826302729528
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 226959.40405396244
+    },
+    {
+      "id": 6,
+      "image_id": 6,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        779.7178423236514,
+        697.51867219917,
+        650.5477178423237,
+        333.49377593361004
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 216953.61484822925
+    },
+    {
+      "id": 7,
+      "image_id": 7,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        1207.6906077348067,
+        1744.4419889502763,
+        1979.2707182320444,
+        491.0055248618782
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 971832.8578492716
+    },
+    {
+      "id": 8,
+      "image_id": 8,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        1794.0000000000002,
+        1548.1875000000002,
+        1470.5625,
+        741.7499999999999
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 1090789.7343749998
+    },
+    {
+      "id": 9,
+      "image_id": 9,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        586.0196078431377,
+        962.0121381886041,
+        862.1391223155921,
+        162.98786181139081
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 140518.2121301675
+    },
+    {
+      "id": 10,
+      "image_id": 10,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        308.12154696132586,
+        663.6464088397786,
+        837.0994475138119,
+        130.363326468866
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 109127.06856315043
+    },
+    {
+      "id": 11,
+      "image_id": 11,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        753.4065620542077,
+        366.5221112696148,
+        590.507845934379,
+        516.8901569186874
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 305227.69314673723
+    },
+    {
+      "id": 12,
+      "image_id": 12,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        677.0386740331487,
+        1787.138121546961,
+        2473.4543417880764,
+        399.6463869157219
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 988507.0908966098
+    },
+    {
+      "id": 13,
+      "image_id": 13,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        1368.018902392689,
+        1303.980148883375,
+        1560.1261312963827,
+        940.1464019851086
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 1466746.9689812413
+    },
+    {
+      "id": 14,
+      "image_id": 14,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        468.5219421101764,
+        176.24631883678342,
+        954.6685340802985,
+        909.1380088461206
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 867925.4501818074
+    },
+    {
+      "id": 15,
+      "image_id": 15,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        643.2997198879551,
+        931.1688211710507,
+        847.4519140989728,
+        142.46609946386968
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 120733.16868487101
+    },
+    {
+      "id": 16,
+      "image_id": 16,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        589.7651933701657,
+        618.7195464032363,
+        982.9419889502765,
+        275.8338605949221
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 271128.68355300603
+    },
+    {
+      "id": 17,
+      "image_id": 17,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        587.4883286647992,
+        962.0121381886041,
+        862.1391223155927,
+        160.0905695611573
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 138020.3431324595
+    },
+    {
+      "id": 18,
+      "image_id": 18,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        268.92092728995175,
+        448.2009925558316,
+        777.1988901306255,
+        280.52109181141424
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 218020.68121406244
+    },
+    {
+      "id": 19,
+      "image_id": 19,
+      "category_id": 0,
+      "segmentation": [],
+      "bbox": [
+        1551.7079530638853,
+        1318.9517601043024,
+        719.8200782268583,
+        290.9452411994785
+      ],
+      "ignore": 0,
+      "iscrowd": 0,
+      "area": 209428.22627994078
+    }
+  ],
+  "info": {
+    "year": 2023,
+    "version": "1.0",
+    "description": "",
+    "contributor": "Label Studio",
+    "url": "",
+    "date_created": "2023-06-13 08:05:53.91923"
+  }
+}
+
+parsed_data = json.dumps(json_data)
+parsed_data = json.loads(parsed_data)
+
+
+
+list_of_dicts = []
+
+for i in range(20):
+    dict_key = f"image_{i+1}"
+    dict_value = {
+        "file_name": parsed_data["images"][i]["file_name"],
+        "area": parsed_data["annotations"][i]["area"],
+        "bbox": parsed_data["annotations"][i]["bbox"]
+    }
+    list_of_dicts.append({dict_key: dict_value})
+
+
+print(list_of_dicts)
+
+json_list_of_dicts = json.dumps(list_of_dicts)
+
+
+json_list_of_dicts 
+
+file_path = '/Users/901004/Documents/Annotations/final.json'
+
+with open(file_path, 'w') as file:
+    file.write(json_list_of_dicts)
